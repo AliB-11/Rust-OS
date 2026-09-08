051e34145dbe7a2315168799fde65b2500c71225
Ali Bandali <alimbandali@gmail.com>
2026-09-08 00:51:52 -0400

    add files

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,65 @@
-// in src/main.rs
-
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(mini_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
+extern crate alloc;
+
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 use mini_os::println;
+use bootloader::{BootInfo, entry_point};
+use core::panic::PanicInfo;
+
+
+entry_point!(kernel_main);
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-     mini_os::init(); // new
-
-    // fn stack_overflow() {
-    //     stack_overflow(); // for each recursion, the return address is pushed
-    // }
-
-    // // trigger a stack overflow
-    // stack_overflow();
-
-
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
    
+    use mini_os::allocator; 
+    use mini_os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+    
+    println!("Hello World{}", "!");
+    mini_os::init();
+    
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+     };
+
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+
+
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+
+    // as before
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-
-    mini_os::hlt_loop();  
+    mini_os::hlt_loop(); 
 }
 
 
@@ -49,3 +79,4 @@ fn panic(info: &PanicInfo) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     mini_os::test_panic_handler(info)
 }
+
